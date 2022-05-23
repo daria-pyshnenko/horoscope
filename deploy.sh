9404e0ab58e8b1c2aad4e4a44e8bc01cc760ece6
Dasha <rogozyanchik@gmail.com>
2022-05-23 17:32:03 +0300

    adding dist subtree

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+##!/usr/bin/env sh
+#set -e
+#npm run build
+#cd dist
+#dit init
+#get add -A
+#git commit -m "New Deployment"
+#git push -f https://github.com/daria-pyshnenko/horoscope master:gh-pages
+#cd-
