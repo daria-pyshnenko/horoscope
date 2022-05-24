97ab4855f11e4e3b7a5243530ffaf9f9965e46d8
Dasha <rogozyanchik@gmail.com>
2022-05-24 18:10:08 +0300

    path for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// const { defineConfig } = require('@vue/cli-service')
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+// export default defineConfig({
+//
+// })
+
+module.exports =   defineConfig({
+  base: '/horoscope/',
+  plugins: [vue()],
+  // publicPath: './dist/', //process.env.NODE_ENV === "production" ? "/horoscope/" : "/",
 })
